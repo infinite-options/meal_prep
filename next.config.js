2fbb90c06ec5891fcc610ae4772c98afeb9c1096
HaoboZ <haobozhang9081@gmail.com>
2020-08-05 13:34:29 -0700

    fixed bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withPWA = require( 'next-pwa' );
 
-const exportsConfig = {
+let exportsConfig = {
 	env: {
 		STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY
 	},
@@ -9,26 +9,13 @@ const exportsConfig = {
 	}
 };
 
-if ( process.env.ANALYZE ) {
-	const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
-	
-	exports.webpack = config => {
-		// Fixes npm packages that depend on `fs` module
-		config.node = {
-			fs: 'empty'
-		};
-		
-		config.plugins.push( new BundleAnalyzerPlugin( {
-			analyzerMode: 'static',
-			analyzerHost: '127.0.0.1',
-			analyzerPort: 8888,
-			openAnalyzer: false
-		} ) );
-		
-		return config;
-	};
+const withBundleAnalyzer = require( '@next/bundle-analyzer' )( {
+	enabled: process.env.ANALYZE === 'true'
+} );
+exportsConfig = withBundleAnalyzer( exportsConfig );
+
+if ( process.env.NODE_ENV === 'production' ) {
+	exportsConfig = withPWA( exportsConfig );
 }
 
-module.exports = process.env.NODE_ENV === 'production' ?
-	withPWA( exportsConfig ) :
-	exportsConfig;
+module.exports = exportsConfig;
